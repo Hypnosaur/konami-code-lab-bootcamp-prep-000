d7af185a17dda725070a1a7623e55305c7986f37
Laura Fretter <fretterlaura@gmail.com>
2019-04-22 09:52:05 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,27 @@ const codes = [
   "a"
 ];
 
+
 function init() {
   // your code here
+  
+  let index = 0;
+
+  window.addEventListener('keydown', checkKeyPress, false);
+  
+  function checkKeyPress(e){
+    const key = e.key;
+    
+    alert("Hi")
+    if (key === codes[index]){
+      index++;
+      
+      if(index === codes.length){
+        alert("Hurray!");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  }
 }
